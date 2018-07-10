124b6700b4545274085fddc84a8581621ba18843
Ivan Mendoza <ivanmendoza2011@hotmail.com>
2018-07-10 10:42:27 +0000

    Subiendo Login

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -8,19 +8,35 @@
 
 namespace AppBundle\Controller;
 
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
     /**
      * @Route("/login", name="login")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request,AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('security/login.html.twig');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
 
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+
+    }
 }
