e29213bd723c6dc957d4bb016d8856e03bc49e76
Ivan Mendoza <ivanmendoza2011@hotmail.com>
2018-07-10 10:01:37 +0000

    Subiendo Proyecto

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivanm
+ * Date: 7/10/2018
+ * Time: 9:59 AM
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function indexAction(Request $request)
+    {
+        
+
+        return $this->render('security/login.html.twig');
+    }
+
+}
