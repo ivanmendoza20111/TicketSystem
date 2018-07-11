52f4e624972d36dd6a0c639ff039035275f4cd55
Ivan Mendoza <ivanmendoza2011@hotmail.com>
2018-07-11 12:39:40 +0000

    Ticket Realizado

--- src/AppBundle/Controller/Employees/EmployeeController.php
+++ src/AppBundle/Controller/Employees/EmployeeController.php
@@ -17,10 +17,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class EmployeeController extends Controller
 {
--- src/AppBundle/Controller/Ticket/TicketController.php
+++ src/AppBundle/Controller/Ticket/TicketController.php
@@ -8,9 +8,14 @@
 
 namespace AppBundle\Controller\Ticket;
 
+use AppBundle\Entity\Ticket;
+use AppBundle\Form\TicketType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class TicketController extends Controller
 {
@@ -19,6 +24,78 @@ class TicketController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('@App\Ticket\ticket.html.twig');
+        $tickets=$this->getDoctrine()->getManager()->getRepository(Ticket::class)->findBy(array('user'=>$this->getUser()));
+
+        return $this->render('@App\Ticket\ticket.html.twig',array(
+            'tickets'=>$tickets
+        ));
+    }
+
+    /**
+     * @Route("/ticket/new", name="ticket_new")
+     */
+    public function newAction(Request $request)
+    {
+        $ticket=new Ticket();
+        $form=$this->createForm(TicketType::class,$ticket);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $ticket->setUser($this->getUser());
+            $ticket->setDate(new \DateTime());
+            $ticket->setStatus('Open');
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ticket);
+            $em->flush();
+            return $this->redirectToRoute('ticket');
+        }
+
+        return $this->render('@App\Ticket\new.html.twig',array(
+            'form'=>$form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/ticket/delete/{id}",options={"expose"=true}, name="remove_ticket")
+     * @Method("GET")
+     * @param Ticket $ticket
+     */
+    public function deleteEmployee(Ticket $ticket)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($ticket);
+        $em->flush();
+        return $this->redirectToRoute('ticket');
+    }
+
+    /**
+     * @Route("/ticket/view/{id}", name="view_ticket", requirements={"id"="\d+"})
+     * @Method("GET")
+     * @param Ticket $ticket
+     * @return Response
+     */
+    public function indexViewEmployee(Ticket $ticket)
+    {
+        return $this->render('@App\Ticket\view.html.twig',
+            array(
+                "ticket" => $ticket
+            ));
+    }
+
+    //RestFul
+    /**
+     * @Route("/rest/ticket/{id}",options={"expose"=true}, name="delete_ticket")
+     * @Method("DELETE")
+     * @param Ticket $ticket
+     * @return Response
+     */
+    public function deleteTicket(Ticket $ticket)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($ticket);
+        $em->flush();
+        return new Response("1");
     }
 }
